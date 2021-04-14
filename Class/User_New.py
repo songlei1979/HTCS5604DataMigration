d0bc0026baff67cb611e402f701db8b62ad8f704
Lei <gabriel_sl19798@hotmail.com>
2021-04-15 00:47:54 +1200

    start to save

--- Class/User_New.py
+++ Class/User_New.py
@@ -2,7 +2,7 @@ from Class.DB import DB
 
 
 class User_New:
-    def __init__(self, userID, firstName, lastName, address, username, password, cityID):
+    def __init__(self, userID, firstName, lastName, address, username, password, cityID, strengthIDs):
         self.userID = userID
         self.firstName = firstName
         self.lastName = lastName
@@ -10,6 +10,7 @@ class User_New:
         self.username = username
         self.password = password
         self.cityID = cityID
+        self.strengthIDs = strengthIDs
 
     def save(self):
         db = DB()
@@ -17,5 +18,12 @@ class User_New:
         cursor.execute(
             "INSERT INTO userDM_new(userID, firstName, lastName, address, username, password, cityID) "
             "VALUES ("+str(self.userID)+",'"+self.firstName+"','"+self.lastName+"','"+self.address+"','"+self.username+"','"+self.password+"',"+str(self.cityID)+")")
+        i = 0
+        while i < len(self.strengthIDs):
+            s_id = self.strengthIDs[i]
+            cursor.execute(
+                "INSERT INTO userStrength_new(userID, strengthID) "
+                "VALUES (" + str(self.userID) + ","+str(s_id)+")")
+            i = i + 1
         db.close()
 

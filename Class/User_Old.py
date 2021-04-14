5a961dc050bfc6db593dc3f577c678c39ad53bab
Lei <gabriel_sl19798@hotmail.com>
2021-04-14 23:02:29 +1200

    add strengths

--- Class/User_Old.py
+++ Class/User_Old.py
@@ -12,6 +12,8 @@ class User_Old:
         self.lastName = userInfo[2]
         self.address = userInfo[3]
         self.cityID = userInfo[4]
+        cursor.execute("select * from userStrength_old where userID = " + str(userID) + "")
+        self.strengths = cursor.fetchall()
         db.close()
 
 

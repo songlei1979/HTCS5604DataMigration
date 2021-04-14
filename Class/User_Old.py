53a7594fbb5618e0e1927122613e4bd7d36f1498
Lei <gabriel_sl19798@hotmail.com>
2021-04-14 23:11:35 +1200

    add strengths

--- Class/User_Old.py
+++ Class/User_Old.py
@@ -12,8 +12,14 @@ class User_Old:
         self.lastName = userInfo[2]
         self.address = userInfo[3]
         self.cityID = userInfo[4]
-        cursor.execute("select * from userStrength_old where userID = " + str(userID) + "")
-        self.strengths = cursor.fetchall()
+        cursor.execute("select strengthID from userStrength_old where userID = " + str(userID) + "")
+        self.strengths = []
+        result = cursor.fetchall()
+        i = 0
+        while i < len(result):
+            self.strengths.append(result[i][0])
+            i = i + 1
         db.close()
 
-
+user = User_Old(1)
+print(user.__dict__)
